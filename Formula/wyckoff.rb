dc5b9d04e66f65da015e74d9a7e99cd4390396e6
YoungCan-Wang <73347006+YoungCan-Wang@users.noreply.github.com>
2026-05-04 12:09:10 +0800

    chore: bump to v0.7.6

--- Formula/wyckoff.rb
+++ Formula/wyckoff.rb
@@ -3,8 +3,8 @@ class Wyckoff < Formula
 
   desc "Wyckoff method quantitative analysis agent for A-shares"
   homepage "https://github.com/YoungCan-Wang/Wyckoff-Analysis"
-  url "https://files.pythonhosted.org/packages/source/y/youngcan-wyckoff-analysis/youngcan_wyckoff_analysis-0.6.2.tar.gz"
-  sha256 "351b247f449fac5894ca861e52d56604fdb1b7aee15a7cc918b637b00f017fe4"
+  url "https://files.pythonhosted.org/packages/source/y/youngcan-wyckoff-analysis/youngcan_wyckoff_analysis-0.7.6.tar.gz"
+  sha256 "cc02b48bcc23fc6a80e38a662cd4477adbd250ab2968a5d94059e1256f755b4c"
   license "AGPL-3.0-only"
 
   depends_on "python@3.11"
